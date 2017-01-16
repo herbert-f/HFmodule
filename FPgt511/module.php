f1e3425d255e0c586f54c1492422a4b94d9dabb7
HerbertF <herbert-f@mail.de>
2017-01-16 13:10:50 +0100

    no message

--- FPgt511/module.php
+++ FPgt511/module.php
@@ -522,7 +522,7 @@ Fingerreader GT511C3
 			$Befehl=$this->GetBuffer("Command");
 			if ($debug) IPS_LogMessage($Name,"Senden: ".$this->ascii2hex($sendestring));
 			while ($Antwort!=$answer) {
-				SPRT_SendText($COM_ID, $sendestring);  //über seriellen Port direkt //sollte nicht verwandt werden (Probleme beim Test unter Windows)
+				//SPRT_SendText($COM_ID, $sendestring);  //über seriellen Port direkt //sollte nicht verwandt werden (Probleme beim Test unter Windows)
 				$resultat = $this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $sendestring)));   // Weiterverarbeiten und durchreichen
 				//$this->SendDataToParent(json_encode(Array("DataID" => "{79827379-F36E-4ADA-8A95-5F8D1DC92FA9}", "Buffer" => $sendestring)));
 				IPS_Sleep($delay);
