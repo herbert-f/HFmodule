c962a2168d6d8143f2033322dcf6c672a465fe1b
HerbertF <herbert-f@mail.de>
2017-06-09 18:16:41 +0200

    Fehlerfix

--- FPgt511/module.php
+++ FPgt511/module.php
@@ -535,8 +535,8 @@ Fingerreader GT511C3
 				$Antwort=$this->GetBuffer("Answer");
 				//
 				if ($debug) IPS_LogMessage($Name,"Senden: Letzter Befehl: $Befehl, Letzte Antwort: ".$this->GetBuffer("Answer"));
-				If (($Antwort!=$answer) && ($Antwort!="NOACK")) 	{
-					if ($debug) IPS_LogMessage($Name,"$functionname- Fehler - kein: $answer oder NOACK erhalten - sondern dies: $Antwort - Starte COM-SS neu!");
+				If (($Antwort!=$answer) && ($Antwort!="NOACK") && ($Antwort!="Begin")) 	{
+					if ($debug) IPS_LogMessage($Name,"$functionname- Fehler unbekannte Antwort: $Antwort - Starte COM-SS neu!");
 					IPS_Sleep($delay);
 					$ErrorCount++;
 					if ($debug) IPS_LogMessage($Name,"$functionname- Fehler - kein: $answer erhalten");
