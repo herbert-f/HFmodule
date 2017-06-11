2ae9658230d40d522b1b89c78288575d49abef83
HerbertF <herbert-f@mail.de>
2017-06-11 17:57:53 +0200

    Fehlerfix

--- FPgt511/module.php
+++ FPgt511/module.php
@@ -266,8 +266,9 @@ Fingerreader GT511C3
 			$Name=IPS_GetName($this->InstanceID);
 			$this->setBuffer("Command","CheckEnrolled");
 			$this->setBuffer("Answer","Begin");
-			$Speicherplatzh=$this->hexToStr(dechex($Speicherplatz));
-			if ($debug) IPS_LogMessage($Name,"CheckEnrolled $Speicherplatz gestartet");                                          
+			//$Speicherplatzh=$this->hexToStr(dechex($Speicherplatz));
+			$Speicherplatzh=$this->int2bytehex($Speicherplatz);
+			if ($debug) IPS_LogMessage($Name,"CheckEnrolled für Speicherplatz $Speicherplatz gestartet");                                          
 			$Command=array("\x21","\x00");										//Command = CheckEnrolled  Parameter =  ID(0~199);
 			$Parameter=array($Speicherplatzh,"\x00","\x00","\x00");
 			$sendestring=$this->buildstring ($Parameter,$Command);
